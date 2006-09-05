ea3565d46f22891cb631a23ff9896374a3e8cf11
Frank Gaede <frank.gaede@desy.de>
2006-09-05 08:52:26 +0000

    added documentation

--- src/cpp/test/mergeXML.cc
+++ src/cpp/test/mergeXML.cc
@@ -2,16 +2,36 @@
 #include "gearxml/MergeXML.h"
 #include "gear/GEAR.h"
 
+
+/** Program to merge two xml files. This can be used if a gear xml 
+ * (that has been automatically created with MokkaGear) is lacking 
+ * some information. XML Elements that are present in only one of the input
+ * files will be merged into the target file. In case the same element
+ * exists in either input file the attribute values from the primary file
+ * are taken. 
+ * 
+ * @author R.Lippe, DESY
+ * @version $Id: mergeXML.cc,v 1.2 2006-09-05 08:52:26 gaede Exp $
+ */
+
+
 int main( int argc, char *argv[] ) {
+
   try{
 
     if( argc < 4 ) {
-      std::cout << "\nMergeXML:   program to join two xml files"
-		<< "\nusage:      MergeXML <devot> <dominant> <target>"
-		<< "\n"
-		<< "\n<devot>     filename for base-file that will be overwritten"
-		<< "\n<dominant>  filename for file that overrules devot file"
-		<< "\n<target>    filename for output file " 
+
+      std::cout << std::endl 
+		<< " Program to merge two xml files. This can be used if a gear xml" << std::endl
+		<< " (that has been automatically created with MokkaGear) is lacking" << std::endl
+		<< " some information. XML Elements that are present in only one of the input" << std::endl
+		<< " files will be merged into the target file. In case the same element"  << std::endl
+		<< " exists in either input file the attribute values from the primary file" << std::endl
+		<< " are taken." << std::endl
+		<< std::endl
+		<< "   <secondary>   filename for base-file that will be overwritten" << std::endl
+		<< "   <primary>     filename for file that overrules secondary file" << std::endl
+		<< "   <target>      filename for output file " << std::endl
 		<< std::endl ;
       exit(1) ;
     }
@@ -24,18 +44,18 @@ int main( int argc, char *argv[] ) {
     gear::MergeXML mergeXML ;
     
     if ( mergeXML.setFile1 (fileName1) ) {
-      std::cout << "devot   : ok" << std::endl ;
+      std::cout << "secondary   : ok" << std::endl ;
     }
     else {
-      std::cout << "devot   : error reading file " << fileName1 << std::endl ;
+      std::cout << "secondary   : error reading file " << fileName1 << std::endl ;
       exit (2) ;
     }
 
     if ( mergeXML.setFile2 (fileName2) ) {
-      std::cout << "dominant: ok" << std::endl ;
+      std::cout << "primary: ok" << std::endl ;
     }
     else {
-      std::cout << "dominant: error reading file " << fileName2 << std::endl ;
+      std::cout << "primary: error reading file " << fileName2 << std::endl ;
       exit (2) ;
     }
 
