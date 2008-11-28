da368b9c91cf6cd523da10441db41cbaf508e7c2
No Author <no.author@desy.de>
2008-11-28 09:50:18 +0000

    This commit was manufactured by cvs2svn to create tag 'v00-06'.

--- src/cpp/gearimpl/include/GearMgrImpl.h
+++ src/cpp/gearimpl/include/GearMgrImpl.h
@@ -16,7 +16,7 @@ namespace gear {
    *  Based on ideas discussed at the 2004 Argonne Simulation Workshop as summarized by T.Behnke.
    *
    * @author F. Gaede, DESY
-   * @version $Id: GearMgrImpl.h,v 1.11 2008-10-22 15:10:46 engels Exp $
+   * @version $Id: GearMgrImpl.h,v 1.5 2007-05-23 10:36:21 gaede Exp $
    */
   class GearMgrImpl : public GearMgr {
 	
@@ -28,13 +28,8 @@ namespace gear {
     GearMgrImpl() ;
     
     /// Destructor.
-    virtual ~GearMgrImpl() ;
+    virtual ~GearMgrImpl() { /* nop */; }
 	
-   /** The unique detector name - typically the model name used in the simulation program
-    */
-    virtual const std::string& getDetectorName() const  throw (UnknownParameterException, std::exception ) ;
-
-
     /** Get named parameters for key. This can be used to describe a subdetector that is not 
      *  yet forseen in the Gear API.
      * 
@@ -67,55 +62,18 @@ namespace gear {
     virtual const CalorimeterParameters & getEcalEndcapParameters() const 
 	throw (UnknownParameterException, std::exception )  ;
 
-    /** Get the Ecal plug parameters.
-     *
-     *  @throws UnknownParameterException
-     */
-    virtual const CalorimeterParameters & getEcalPlugParameters() const 
-	throw (UnknownParameterException, std::exception )  ;
-
-      /** Get the Yoke barrel parameters.
-     *
-     *  @throws UnknownParameterException
-     */
-    virtual const CalorimeterParameters & getYokeBarrelParameters() const 
-	throw (UnknownParameterException, std::exception ) ;
-
-    /** Get the Yoke endcap parameters.
-     *
-     *  @throws UnknownParameterException
-     */
-    virtual const CalorimeterParameters & getYokeEndcapParameters() const 
-	throw (UnknownParameterException, std::exception )  ;
-
-    /** Get the Yoke plug parameters.
-     *
-     *  @throws UnknownParameterException
-     */
-    virtual const CalorimeterParameters & getYokePlugParameters() const 
-	throw (UnknownParameterException, std::exception )  ;
-
-   /** Get the Hcal barrel parameters.
+    /** Get the Hcal barrel parameters.
      *
      *  @throws UnknownParameterException
      */
     virtual const CalorimeterParameters & getHcalBarrelParameters() const 
 	throw (UnknownParameterException, std::exception )  ;
 
-
     /** Get the Hcal endcap parameters.
      *
      *  @throws UnknownParameterException
      */
     virtual const CalorimeterParameters & getHcalEndcapParameters() const 
-	throw (UnknownParameterException, std::exception )  ;
-
-
-    /** Get the Hcal ring parameters.
-     *
-     *  @throws UnknownParameterException
-     */
-    virtual const CalorimeterParameters & getHcalRingParameters() const 
 	throw (UnknownParameterException, std::exception )  ;
 
 
@@ -126,20 +84,6 @@ namespace gear {
     virtual const CalorimeterParameters & getLcalParameters() const 
       throw (UnknownParameterException, std::exception ) ;
     
-    /** Get the LHcal parameters.
-     *
-     *  @throws UnknownParameterException
-     */
-    virtual const CalorimeterParameters & getLHcalParameters() const 
-      throw (UnknownParameterException, std::exception ) ;
- 
-    /** Get the BeamCal parameters.
-     *
-     *  @throws UnknownParameterException
-     */
-    virtual const CalorimeterParameters & getBeamCalParameters() const 
-      throw (UnknownParameterException, std::exception ) ;
- 
     /** Get the VXD parameters.
      *
      *  @throws UnknownParameterException
@@ -175,8 +119,6 @@ namespace gear {
     virtual const std::vector<std::string>  & getGearParameterKeys() const ;
 
 
-    virtual void setDetectorName(const std::string& name) { _detectorName = name ; }
-
     /** Set the GearParameters for the given key - overwrites any 
      *  existing entries.
      */
@@ -195,24 +137,7 @@ namespace gear {
      */
     virtual void setEcalEndcapParameters( CalorimeterParameters* ecalEndcapParameters ) ;
     
-    /** Set the EcalPlugParameters.
-     */
-    virtual void setEcalPlugParameters( CalorimeterParameters* ecalPlugParameters ) ;
-    
-    /** Set the YokeBarrelParameters.
-     */
-    virtual void setYokeBarrelParameters( CalorimeterParameters* yokeBarrelParameters ) ;
-
-    /** Set the YokeEndcapParameters.
-     */
-    virtual void setYokeEndcapParameters( CalorimeterParameters* yokeEndcapParameters ) ;
-    
-    /** Set the YokePlugParameters.
-     */
-    virtual void setYokePlugParameters( CalorimeterParameters* yokePlugParameters ) ;
- 
-	
-	/** Set the HcalBarrelParameters.
+    /** Set the HcalBarrelParameters.
      */
     virtual void setHcalBarrelParameters( CalorimeterParameters* hcalBarrelParameters ) ;
 
@@ -220,21 +145,9 @@ namespace gear {
      */
     virtual void setHcalEndcapParameters( CalorimeterParameters* hcalEndcapParameters ) ;
 
-    /** Set the HcalRingParameters.
-     */
-    virtual void setHcalRingParameters( CalorimeterParameters* hcalRingParameters ) ;
-
     /** Set the LcalParameters.
      */
     virtual void setLcalParameters(CalorimeterParameters * lcalParameters) ;
-
-    /** Set the LHcalParameters.
-     */
-    virtual void setLHcalParameters(CalorimeterParameters * lhcalParameters) ;
-
-    /** Set the BeamCalParameters.
-     */
-    virtual void setBeamCalParameters(CalorimeterParameters * beamcalParameters) ;
 
      /** Set the VXDParameters.
      */
@@ -266,22 +179,14 @@ namespace gear {
     TPCParameters* _tpcParameters ;
     CalorimeterParameters* _ecalBarrelParameters ;
     CalorimeterParameters* _ecalEndcapParameters ;
-    CalorimeterParameters* _ecalPlugParameters ;
-    CalorimeterParameters* _yokeBarrelParameters ;
-    CalorimeterParameters* _yokeEndcapParameters ;
-    CalorimeterParameters* _yokePlugParameters ;
     CalorimeterParameters* _hcalBarrelParameters ;
     CalorimeterParameters* _hcalEndcapParameters ;
-    CalorimeterParameters* _hcalRingParameters ;
     CalorimeterParameters* _lcalParameters ;
-    CalorimeterParameters* _lhcalParameters ;
-    CalorimeterParameters* _beamcalParameters ;
     VXDParameters* _vxdParameters ;
     SiPlanesParameters* _siplanesParameters ;
     GearPointProperties*  _pointProperties ;
     GearDistanceProperties*  _distanceProperties ;
     BField* _bField ;
-    std::string _detectorName ;
 
     mutable StringVec _keys ;
 
