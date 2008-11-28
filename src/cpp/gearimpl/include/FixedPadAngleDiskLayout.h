a823e5b4b36e1a989a4956db8aacdbba8fcc73c6
No Author <no.author@desy.de>
2008-11-28 09:50:37 +0000

    This commit was manufactured by cvs2svn to create tag 'v00-11-01'.

--- src/cpp/gearimpl/include/FixedPadAngleDiskLayout.h
+++ src/cpp/gearimpl/include/FixedPadAngleDiskLayout.h
@@ -2,29 +2,37 @@
 #define FixedPadAngleDiskLayout_h 1
 
 #include <vector>
-#include "gearimpl/FixedDiskLayoutBase.h"
+#include "gear/PadRowLayout2D.h"
 
 
 namespace gear {
 
+
+
 /** Implementation of PadRowLayout2D for a disk with fixed angled keystone pads.
  *  The number of pads is distrubuted on a wedge shaped segment starting 
- *  with the right edge of the first pad in the row at phiMin to the left edge
- *  of the last pad in the row at phiMax.
+ *  with the right edge of the first pad in throw at phiMin and the left edge
+ *  of the last pad in the row.
  *  All rows have the same height, starting at rmin for the lower edge of the
- *  first row to rmax for the upper edge of the last row. The row height is 
- *  calculated from the number of pads.
+ *  first row to rmax for the upper edge of the last row.
  *
  *  Row height and pad height are identical, and pad gap is 0
- *  (the only thing that matters is the effective pitch).
+ *  (the only thing that matters is the effective pitch)
  * 
  * @author M. Killenberg, University of Bonn
- * @version $Id: FixedPadAngleDiskLayout.h,v 1.2 2008-11-28 09:50:09 engels Exp $
+ * @version $$
  */
-  class FixedPadAngleDiskLayout : public FixedDiskLayoutBase {
+  class FixedPadAngleDiskLayout : public PadRowLayout2D {
     
   public:
 
+//    /** Internal helper class for FixedPadAngleDiskLayout */
+//    struct Row{
+//      int NPad ;
+//      double RCenter ;
+//      double PhiPad ;
+//    } ;
+    
 
   protected:
     double _rMin ;
@@ -39,20 +47,19 @@ namespace gear {
     double _padAngle;
     int _nPadsInRow;
     int _nPad ;
-
+//    std::vector<Row> _rows ;
     std::vector<double> _extent ;
     mutable std::vector< std::vector<int>* > _padIndices ;
 
-  /** Function to copy all internal variables, incl.\ the objects
-   *  pointed to and owned by the FixedPadAngleDiskLayout.
-   *
-   *  Used by constructor and assignment operator to avoid code duplication.
+  /** function to copy all internal variables, incl. the objects
+   *  pointed to and owned by the ModularTPCParameters.
+   *  Used by constructor and assigment operator to avoid code duplication
    */
   void copy_and_assign(const  FixedPadAngleDiskLayout & );
 
-  /** Function to delete all the objects
-   *  pointed to and owned by the FixedPadAngleDiskLayout.
-   *  Used by destructor and assignment operator to avoid code duplication.
+  /** function to delete all the objects
+   *  pointed to and owned by the GearMgr.
+   *  Used by desctructor and assigment operator to avoid code duplication
    */
   void cleanup();
     
@@ -60,7 +67,9 @@ namespace gear {
   public: 
 
     /** Construct the FixedPadAngleDiskLayout from the given parameters rMin, rMax,
-     *  nRow, phiMin, phiMax, and nPadsInRow.
+     *  padHeight and PadWidth where the maximum number of pads are fitted to the plane.
+     *  @param nRow :  limits the number of rows - if given 
+     *  @param padGap : additional gap between pads in r-phi in mm 
      */
     
   FixedPadAngleDiskLayout( double   rMin, double   rMax, int nRow,
@@ -78,7 +87,7 @@ namespace gear {
     virtual ~FixedPadAngleDiskLayout() ; 
     
 
-    /** The gap width in mm that was given in the C'tor. Always 0 in this implementation. */
+    /** The gap width in mm that was given in the C'tor. */
     virtual double getPadGap() const { return 0 ; } 
 
     /** The fixed width of the pads in mm. 
@@ -86,17 +95,13 @@ namespace gear {
     virtual double getFixedPadAngle() const { return _padAngle ; }
     
 
-    /* The type of the row layout: PadRowLayout2D::POLAR.
+    /** The type of the row layout: PadRowLayout2D::POLAR.
      */
-    //
-    // Already implemented in FixedDiskLayoutBase
-    //virtual int getPadLayoutType() const { return PadRowLayout2D::POLAR ; } 
+    virtual int getPadLayoutType() const { return PadRowLayout2D::POLAR ; } 
     
-    /* The shape of the pads: PadRowLayout2D::RECTANGLE (i.e.\ keystone).
+    /** The shape of the pads: PadRowLayout2D::RECTANGLE (i.e. keystone).
      */ 
-    //
-    // Already implemented in FixedDiskLayoutBase
-    //virtual int getPadShape() const { return PadRowLayout2D::RECTANGLE ; }
+    virtual int getPadShape() const { return PadRowLayout2D::RECTANGLE ; }
     
     /** The total number of pads in the TPC.
      */
@@ -118,41 +123,36 @@ namespace gear {
      */
     virtual double getPadWidth(int padIndex) const { return _padAngle;}
 
-    /** The center of the pad in 2d coordinates (r,phi).
+    /** The center of the pad in 2d coordinates, (x,y) or (r,phi).
      */
     virtual Vector2D getPadCenter(int padIndex)  const;
 
-    /** Get the (fixed) number of pads per row.
-     *  Just returns the value set in the constructor.
-     */
     virtual int getNPadsInRow() const {return _nPadsInRow; }
 
-    /** Indices of all pads in row rowNumber (row indices start at rMin). 
+    /** Indices of all pads in row rowNumber (row indices start from 0 at the
+     * bottom (CARTESIAN) or at the center (POLAR)). 
      */
     virtual const std::vector<int>& getPadsInRow(int rowNumber) const ;
 
-    /** Extent of the sensitive plane [rmin,rmax,phimin,phimax].
+    /** Extent of the sensitive plane - [xmin,xmax,ymin,ymax] CARTESIAN or 
+     *	[rmin,rmax,phimin,phimax] POLAR.
      */
     virtual const std::vector<double>& getPlaneExtent()  const { return _extent ; }
 
-    /* The number of the row that contains the pad at padIndex.
-     *  Numbering starts at rMin and phiMin.
+    /** The number of the row that contains the pad at padIndex - numbering starts at r/y==0.
      */
-    //
-    // Already implemented in FixedDiskLayoutBase
-    //virtual int getRowNumber(int padIndex)  const { return ( 0xffff0000 & padIndex ) >> 16 ; } 
+    virtual int getRowNumber(int padIndex)  const { return ( 0xffff0000 & padIndex ) >> 16 ; } 
 
-    /* The pad number (column) within the row  - numbering starts at phi/x =.
+    /** The pad number (column) within the row  - numbering starts at phi/x =.
      */
-    //
-    // Already implemented in FixedDiskLayoutBase
-    //virtual int getPadNumber(int padIndex) const { return ( 0x0000ffff & padIndex ) ; }
+    virtual int getPadNumber(int padIndex) const { return ( 0x0000ffff & padIndex ) ; }
 
     /** Create a padIndex for the given row and pad ( column ) number 
      */
     virtual int getPadIndex(int rowNum, int padNum)  const;
 
-    /** The index of the pad nearest to the given point in 2d coordinates (r, phi).
+    /** The index of the pad nearest to the given point in 2d coordinates
+     * (x,y,) or (r,phi).
      */
     virtual int getNearestPad(double c0, double c1)  const;
 
@@ -172,12 +172,6 @@ namespace gear {
      */
     virtual bool isInsidePad(double c0, double c1)  const;
 
-    /* Returns the closest distance to the edge of the pad
-     */
-        //
-    // Already implemented in FixedDiskLayoutBase
-    //virtual double getDistanceToPad(double c0, double c1, int padIndex) const;
-    
   }; // class
 
 } // namespace gear
