55dd3d25793c3f21b49f1f0ed646d920b63bf30e
Astrid Munnich <astrid.muennich@cern.ch>
2010-07-16 11:48:55 +0000

    testMaterialBudget now calculates material budget before TPC, TPC and up to ECAL

--- src/test/testMaterialBudget.cc
+++ src/test/testMaterialBudget.cc
@@ -38,7 +38,9 @@ void gear_unexpected(){
 }
 
 
-/** Testprogram for gear classes.
+/** Program uses geartgeo and geometry info in gear file
+ *  to compute material budget for everything before the TPC,
+ *  the TPC alone and everything up to the ecal front.
  * 
  */
 
@@ -61,7 +63,7 @@ int main(int argc, char**argv){
   GearMgr* gearMgr = gearXML.createGearMgr() ;
   
   ///-----TPC----------------------------------------------------------------
-  double L_tpc=0, Rtpc_i=0,Rtpc_o=0, Recal=0;
+  double L_tpc=0, Rtpc_i=0,Rtpc_o=0, Recal_o=0,Recal_i=0, Zecal=0;
   try{
     const TPCParameters &tpcparams= gearMgr->getTPCParameters() ;
     //convert to cm for TGeo
@@ -84,56 +86,88 @@ int main(int argc, char**argv){
     std::cout << "  oops - no TPC_Ecal_Hcal_barrel_halfZ available :( " << std::endl ;
   }
   try{
-    const CalorimeterParameters &ecalparams= gearMgr->getEcalBarrelParameters() ;
+    const CalorimeterParameters &ecalparamsB= gearMgr->getEcalBarrelParameters() ;
     //convert to cm for TGeo
-    Recal =  ecalparams.getExtent()[0]/10.;
-    std::cout<<ecalparams.getExtent()[0]<<" "<<ecalparams.getExtent()[1]<<" "
-	     <<ecalparams.getExtent()[1]<<" "<<ecalparams.getExtent()[2]<<std::endl;
+    Recal_o =  ecalparamsB.getExtent()[0]/10.;
   }
   catch( gear::UnknownParameterException& e ){
-    std::cout << "  oops - no Ecal available :( " << std::endl ;
+    std::cout << "  oops - no Ecal barrel available :( " << std::endl ;
   }
   try{
-    const CalorimeterParameters &ecalparams= gearMgr->getEcalEndcapParameters() ;
+    const CalorimeterParameters &ecalparamsEC= gearMgr->getEcalEndcapParameters() ;
     //convert to cm for TGeo
-    std::cout<<ecalparams.getExtent()[0]<<" "<<ecalparams.getExtent()[1]<<" "
-	     <<ecalparams.getExtent()[1]<<" "<<ecalparams.getExtent()[2]<<std::endl;
+    Zecal=ecalparamsEC.getExtent()[2]/10;
   }
   catch( gear::UnknownParameterException& e ){
-    std::cout << "  oops - no Ecal available :( " << std::endl ;
+    std::cout << "  oops - no Ecal endcap available :( " << std::endl ;
   }
-  
+  try{
+    const CalorimeterParameters &ecalparamsPl= gearMgr->getEcalPlugParameters() ;
+    //convert to cm for TGeo
+    Recal_i=ecalparamsPl.getExtent()[0]/10;
+  }
+  catch( gear::UnknownParameterException& e ){
+    std::cout << "  oops - no Ecal endcap available :( " << std::endl ;
+  }
   
   try{
     TGeoGearDistanceProperties &distProp = (TGeoGearDistanceProperties&)gearMgr->getDistanceProperties();
-    std::vector<std::string> volNames;
-    Vector3D initial, final;
-   
+    Vector3D tpc_initial, tpc_final;
+    Vector3D all_initial, all_final;
+    Vector3D bf_initial, bf_final;
+    double tpc_radLen=0, all_radLen=0, bf_radLen=0;
+    std::cout<<"# theta      before TPC      TPC        up to ECAL"<<std::endl;
     for(int theta=0;theta<90;theta++)
       {
-	initial[0] = 0;
-	initial[1] = Rtpc_i;
-	initial[2] = Rtpc_i/tan(theta*M_PI/180);
-	  
-	final[0] = 0.0;
-	final[1] = Rtpc_o;
-	final[2] = Rtpc_o/tan(theta*M_PI/180);
-	if(initial[2]>L_tpc)
-	  continue;
-	if(final[2]>L_tpc)
+	//all up to ECAL front
+	all_initial[0] = 0;
+	all_initial[1] = 0;
+	all_initial[2] = 0;
+	all_final[0] = 0.0;
+	all_final[1] = Recal_o;
+	all_final[2] = Recal_o/tan(theta*M_PI/180);
+	if(all_final[2]>Zecal)
 	  {
-	    final[2]=L_tpc;
-	    final[1]=L_tpc*tan(theta*M_PI/180);
+	    all_final[2]=Zecal;
+	    all_final[1]=Zecal*tan(theta*M_PI/180);
 	  }
+	if(all_final[1]<Recal_i)
+	  all_radLen=0;
+	else
+	  all_radLen=distProp.getNRadlen(all_initial, all_final);
+	//TPC only
+	tpc_initial[0] = 0;
+	tpc_initial[1] = Rtpc_i;
+	tpc_initial[2] = Rtpc_i/tan(theta*M_PI/180);
+	  
+	tpc_final[0] = 0.0;
+	tpc_final[1] = Rtpc_o;
+	tpc_final[2] = Rtpc_o/tan(theta*M_PI/180);
+	if(tpc_final[2]>L_tpc)
+	  {
+	    tpc_final[2]=L_tpc;
+	    tpc_final[1]=L_tpc*tan(theta*M_PI/180);
+	  }
+	if(tpc_initial[2]>L_tpc)
+	  tpc_radLen=0;
+	else
+	  tpc_radLen=distProp.getNRadlen(tpc_initial, tpc_final);
+	
+	//before TPC
+	bf_initial[0] = 0;
+	bf_initial[1] = 0;
+	bf_initial[2] = 0;
+	bf_final[0] = 0.0;
+	bf_final[1] = tpc_initial[1];
+	bf_final[2] = tpc_initial[2];
+	if(bf_final[2]>L_tpc)
+	  bf_radLen=0;
+	else
+	  bf_radLen=distProp.getNRadlen(bf_initial, bf_final);
 
-	volNames = distProp.getVolumeNames(initial, final);
-	double radLen=distProp.getNRadlen(initial, final);
+	std::cout<<theta<<" "<<bf_radLen<<" "<<tpc_radLen<<" "<<all_radLen<<std::endl;
 
-	std::cout<<theta<<" "<<radLen<<std::endl;
-// 	for(unsigned int i=0; i<volNames.size();i++)
-// 	  std::cout <<"# "<<initial[2]<<" "<<final[2]<<" "<<volNames[i].c_str()<< std::endl;
-// 	for(unsigned int i=0; i<volNames0.size();i++)
-// 	  std::cout <<"# "<<startZero[2]<<" "<<final[2]<<" "<<volNames0[i].c_str()<< std::endl;
+
 	
       }
   }
