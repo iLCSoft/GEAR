8afc191bf6087e6d8fa3d563b1e92a6a4fcdf514
scott snyder <sss@karma>
2020-02-01 20:58:27 -0500

    Fix compiler warnings about catching polymorphic type by value.

--- src/test/testGearTGeo.cc
+++ src/test/testGearTGeo.cc
@@ -88,7 +88,7 @@ int main(int argc, char**argv){
       std::cout <<i<<" "<< volNames[i]<<" "<<matNames[i] << std::endl;
       
   }
-  catch(NotImplementedException e){}
+  catch(const NotImplementedException& e){}
   
  
 
--- src/test/testMaterialMap.cc
+++ src/test/testMaterialMap.cc
@@ -82,7 +82,7 @@ int main(int argc, char**argv){
 	  }
       }
   }
-  catch(NotImplementedException e){}
+  catch(const NotImplementedException& e){}
   
  
 
